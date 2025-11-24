fb1273f47f928914e63b925253421f14110332ff
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2025-11-24 10:24:52 -0500

    Feat: fetching data from JSON and rendering it on the DOM

---
+++ product-page/products.js
@@ -0,0 +1,22 @@
+fetch("../data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.data);
+    displayData(data.data);
+});
+
+function displayData(data) {
+  const productSec = document.querySelector('.product-sec');
+  data.forEach( product => {
+    console.log(product);
+    let productCard = `
+    <div class='product-sec__card' id='${product.id}'>
+      <img src='${product.attributes.img_url}' alt='${product.attributes.name}'>
+      <h2>${product.attributes.name}</h2>
+      <p>$${product.attributes.price.toFixed(2)}</p>
+      <p>${product.attributes.description}</p>
+    </div>
+    `;
+    productSec.insertAdjacentHTML('beforeend', productCard);
+  });
+}
