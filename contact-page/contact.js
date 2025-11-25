ac4003100ab4729a14f7d5a773573a7a803e31d8
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2025-11-25 09:11:59 -0500

    Feat: added socials to footer

--- contact-page/contact.js
+++ contact-page/contact.js
@@ -2,6 +2,12 @@ const submitBtn = document.getElementById('submit-btn');
 const commentBox = document.getElementById('comment-box');
 const characterOutput = document.getElementById('character-output');
 
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+
+const errorBox = document.querySelector('.error-message');
+
 characterOutput.textContent = '0 / 500';
 
 function openNav() {
@@ -14,9 +20,32 @@ function closeNav() {
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  validateForm();
 })
 
 commentBox.addEventListener('input', () => {
   const currentLength = commentBox.value.length;
   characterOutput.textContent = `${currentLength} / 500`;
 });
+
+function validateForm() {
+  if (
+    firstName.value == '' ||
+    lastName.value == '' ||
+    email.value == '' ||
+    commentBox.value == ''
+  ) {
+    e.preventDefault();
+    errorBox.textContent = 'Please fill out all required forms.';
+  } else {
+    alert('Thank you for reaching out to Bagel Bites! We will get back to you shortly.');
+    clearForm();
+  }
+}
+
+function clearForm() {
+  firstName.value = '';
+  lastName.value = '';
+  email.value = '';
+  commentBox.value = '';
+}
