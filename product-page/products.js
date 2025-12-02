010df3d29a5f1b949ce66c0d8a2cb06c22d9e524
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2025-12-02 14:42:16 -0500

    Fix: removed console log

--- product-page/products.js
+++ product-page/products.js
@@ -1,14 +1,12 @@
 fetch("../data.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data.data);
     displayData(data.data);
 });
 
 function displayData(data) {
   const productSec = document.querySelector('.product-sec');
   data.forEach( product => {
-    console.log(product);
     let productCard = `
     <div class='product-sec__card' id='${product.id}'>
       <img src='${product.attributes.img_url}' alt='${product.attributes.name}'>
